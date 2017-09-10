8777d30a3a8fb789933ec1ccd2706fbee98a3545
Jacqueen <jaqlin_mardirossian@edu.aua.am>
2017-09-10 12:02:07 +0400

    homework

---
+++ homework2.js
@@ -0,0 +1,17 @@
+var myAge = function(year, yearOfBirth)
+{ var age = (year - yearOofBirth);
+return age;
+};
+var result = myAge(2017,1999);
+console.log(result);
+
+if (myAge>=18) {
+console.log("Congrats you're an adult");
+}
+else {
+ console.log("Enjoy your free life!");
+}
+var name = 'Jaqlin';
+var surname = 'Mardirossian';
+var fullName = 'Jaqlin' + 'Mardirossian';
+console.log(fullName);
